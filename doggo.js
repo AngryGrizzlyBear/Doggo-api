a1dac6a4d1325d54da945c3c9cb5f7eb0960e75a
Evan Longwood <evanmlongwood@gmail.com>
2019-04-09 22:11:16 -0700

    Getting broken images, but I'm making progress.

--- doggo.js
+++ doggo.js
@@ -1,13 +1,33 @@
-let request = new XMLHttpRequest();
+const userAction = async () => {
+    const response = await fetch('https://random.dog/woof.json');
+        const myJson = await response.json();
+        console.log(myJson)
 
-request.open('GET', "https://random.dog/woof.json", true);
-request.onload = function() {
-// Begin accessing JSON data here
-    var data = JSON.parse(this.response);
-    console.log(data)
 };
 
-request.send();
+
+function button () {
+    document.getElementById('btn').onclick = function () {
+        let src = userAction(),
+            img = document.createElement('img');
+
+        img.src = src;
+        document.body.appendChild(img);
+    };
+}
+
+
+// let request = new XMLHttpRequest();
+//
+// request.open('GET', "https://random.dog/woof.json", true);
+// request.onload = function() {
+// // Begin accessing JSON data here
+//     let data = JSON.parse(this.response);
+//     console.log(data)
+// };
+//
+//
+// request.send();
 //
 // const getJSON(,
 //     function(data) {
