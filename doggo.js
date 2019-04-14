b0f3d84363192af8410e341719be79089652a0eb
Evan Longwood <evanmlongwood@gmail.com>
2019-04-14 15:16:24 -0700

    Now replaces webM, video, and img from the api.

--- doggo.js
+++ doggo.js
@@ -8,57 +8,24 @@ const userAction = async () => {
 function button() {
     document.getElementById('btn').onclick = async function () {
         const src = await userAction();
-        const img = document.createElement('img');
-        img.src = src;
-        img.width = 500;
-        img.height = 500;
-        img.alt = "doggo";
-        img.id = 'doggo_image';
         let pieces = src.split(".");
+        document.getElementById('media').innerHTML = "";
 
-        // Need to figure this part out
-        if (pieces[pieces.length-1] === "mp4" || pieces[pieces.length-1] === "webM") {
-            let video = document.createElement("video");
+
+
+        if (pieces[pieces.length - 1] === "mp4" || pieces[pieces.length - 1] === "webM") {
+            const video = document.createElement("video");
             video.src = src;
-            document.body.append(video);
+            video.id = 'doggo_image';
+            document.getElementById('media').appendChild(video);
         } else {
-
-        }
-
-        const current_image = document.getElementById(img.id);
-        if (current_image) {
-            document.body.replaceChild(img, current_image);
-        } else {
-            document.body.appendChild(img);
+            const img = document.createElement('img');
+            img.src = src;
+            img.width = 500;
+            img.height = 500;
+            img.alt = "doggo";
+            img.id = 'doggo_image';
+            document.getElementById('media').appendChild(img);
         }
     };
 }
-
-// function button () {
-//     document.getElementById('btn').onclick = function () {
-//         let src = userAction(),
-//             img = document.createElement('img');
-//         console.log(src);
-//         img.src = src;
-//         document.body.appendChild(img);
-//     };
-// }
-
-
-// let request = new XMLHttpRequest();
-//
-// request.open('GET', "https://random.dog/woof.json", true);
-// request.onload = function() {
-// // Begin accessing JSON data here
-//     let data = JSON.parse(this.response);
-//     console.log(data)
-// };
-//
-//
-// request.send();
-//
-// const getJSON(,
-//     function(data) {
-//     console.log(data);
-// });
-
