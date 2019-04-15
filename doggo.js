07a480e8900f4b8955208d269c85d65b5cc2405f
Evan Longwood <evanmlongwood@gmail.com>
2019-04-15 15:12:40 -0700

    refactored some code and made some changes. Function now loads on one click instead of two.

--- doggo.js
+++ doggo.js
@@ -5,36 +5,30 @@ const userAction = async () => {
 };
 
 
-function button() {
-    document.getElementById('btn').onclick = async function () {
-        const src = await userAction();
-        let pieces = src.split(".");
-        document.getElementById('media').innerHTML = "";
+async function button() {
+    const src = await userAction();
+    let pieces = src.split(".");
+    document.getElementById('media').innerHTML = "";
 
-        if (pieces[pieces.length - 1] === "mp4" || pieces[pieces.length - 1] === "webM") {
-            const video = document.createElement("video");
-            video.src = src;
-            video.id = 'doggo_image';
-            video.width = 500;
-            video.height = 500;
-            video.alt = "doggotoo";
-            document.getElementById('media').appendChild(video);
-        } else {
-            const img = document.createElement('img');
-            img.src = src;
-            img.width = 500;
-            img.height = 500;
-            img.alt = "doggo";
-            img.id = 'doggo_image';
-            document.getElementById('media').appendChild(img);
-        }
-    };
-    change()
+    if (pieces[pieces.length - 1] === "mp4" || pieces[pieces.length - 1] === "webM") {
+        const video = document.createElement("video");
+        video.src = src;
+        video.id = 'doggo_image';
+        video.width = 500;
+        video.height = 500;
+        document.getElementById('media').appendChild(video);
+    } else {
+        const img = document.createElement('img');
+        img.src = src;
+        img.width = 500;
+        img.height = 500;
+        document.getElementById('media').appendChild(img);
+    }
+    change();
 }
 
-function change() // no ';' here
-{
-    var elem = document.getElementById("btn");
-    if (elem.textContent==="Click Me!") elem.textContent = "Click Me!";
+function change() {
+    const elem = document.getElementById("btn");
+    if (elem.textContent === "Click Me!") elem.textContent = "Click Me!";
     else elem.textContent = "Click Me Again!";
 }
